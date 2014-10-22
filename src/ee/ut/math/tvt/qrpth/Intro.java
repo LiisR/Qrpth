cac0630d2752f02252e819b7ac2a54c278da973e
LiisR <liisreisberg86@gmail.com>
2014-10-22 20:17:29 +0300

    Eeltöö

--- src/ee/ut/math/tvt/qrpth/Intro.java
+++ src/ee/ut/math/tvt/qrpth/Intro.java
@@ -1,20 +1,24 @@
 package ee.ut.math.tvt.qrpth;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import java.net.URL;
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 import ee.ut.math.tvt.qrpth.IntroUI;
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
 	private static final Logger log = LogManager.getLogger(Intro.class);
+	private static final String MODE = "console";
+
+
 	
 	@SuppressWarnings("resource")
 	static InputStream getResource(String name) throws FileNotFoundException {
@@ -30,10 +34,38 @@ public class Intro {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		final IntroUI ui = new IntroUI();
-		ui.setVisible(true);
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
 		
-		log.info("Team intro started");
+		
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			//final IntroUI ui = new IntroUI();
+			//ui.setVisible(true);
+			
+			
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+			
+		}
+		
+		
 	}
 	
 }
---
+++ src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemModel.java
@@ -0,0 +1,46 @@
+package ee.ut.math.tvt.salessystem.ui.model;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+
+/**
+ * Main model. Holds all the other models.
+ */
+public class SalesSystemModel {
+    
+    private static final Logger log = LogManager.getLogger(SalesSystemModel.class);
+
+    // Warehouse model
+    private StockTableModel warehouseTableModel;
+    
+    // Current shopping cart model
+    private PurchaseInfoTableModel currentPurchaseTableModel;
+
+    private final SalesDomainController domainController;
+
+    /**
+     * Construct application model.
+     * @param domainController Sales domain controller.
+     */
+    public SalesSystemModel(SalesDomainController domainController) {
+        this.domainController = domainController;
+        
+        warehouseTableModel = new StockTableModel();
+        currentPurchaseTableModel = new PurchaseInfoTableModel();
+
+        // populate stock model with data from the warehouse
+        warehouseTableModel.populateWithData(domainController.loadWarehouseState());
+
+    }
+
+    public StockTableModel getWarehouseTableModel() {
+        return warehouseTableModel;
+    }
+
+    public PurchaseInfoTableModel getCurrentPurchaseTableModel() {
+        return currentPurchaseTableModel;
+    }
+    
+}
