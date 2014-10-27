35742c8f269b86e12677aaf91c1658106d9e941a
Anne-Mai Ilumäe <ami@ut.ee>
2014-10-27 23:54:01 +0200

    HistoryTable model_unfinished

--- src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
+++ src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
@@ -1,22 +1,45 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
 import java.awt.Component;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 
 /**
  * Encapsulates everything that has to do with the purchase tab (the tab
  * labelled "History" in the menu).
  */
 public class HistoryTab {
-    
-    // TODO - implement!
+	   private SalesSystemModel model;
+	    JTable historyTable;
+//konstruktor	   
+	    public HistoryTab(SalesSystemModel model) {
+	        this.model = model;
+	    } 
 
-    public HistoryTab() {} 
-    
+   
     public Component draw() {
         JPanel panel = new JPanel();
         // TODO - Sales history tabel
         return panel;
     }
+    
+    JPanel mainPanel = new JPanel(new GridLayout(2,1));
+  
+   
+  
+   
+    
+
+
+
+   
+
+
+
 }
